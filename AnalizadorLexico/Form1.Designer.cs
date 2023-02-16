c74213f5d15d7d1977aa6c625e113e64eb9d63a5
ERIKA TAMARA GARCIA <oscary_1320114020@uptecamac.edu.mx>
2023-02-16 17:51:36 -0600

    16-02-23

---
+++ AnalizadorLexico/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AnalizadorLexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.interpretar_btn = new System.Windows.Forms.Button();
+            this.listBox_variablesEncontradas = new System.Windows.Forms.ListBox();
+            this.texto_richText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // interpretar_btn
+            // 
+            this.interpretar_btn.Location = new System.Drawing.Point(91, 605);
+            this.interpretar_btn.Name = "interpretar_btn";
+            this.interpretar_btn.Size = new System.Drawing.Size(75, 23);
+            this.interpretar_btn.TabIndex = 1;
+            this.interpretar_btn.Text = "Interpretar";
+            this.interpretar_btn.UseVisualStyleBackColor = true;
+            this.interpretar_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox_variablesEncontradas
+            // 
+            this.listBox_variablesEncontradas.FormattingEnabled = true;
+            this.listBox_variablesEncontradas.HorizontalScrollbar = true;
+            this.listBox_variablesEncontradas.Location = new System.Drawing.Point(568, 404);
+            this.listBox_variablesEncontradas.MultiColumn = true;
+            this.listBox_variablesEncontradas.Name = "listBox_variablesEncontradas";
+            this.listBox_variablesEncontradas.ScrollAlwaysVisible = true;
+            this.listBox_variablesEncontradas.Size = new System.Drawing.Size(236, 277);
+            this.listBox_variablesEncontradas.TabIndex = 2;
+            // 
+            // texto_richText
+            // 
+            this.texto_richText.Location = new System.Drawing.Point(28, 42);
+            this.texto_richText.Name = "texto_richText";
+            this.texto_richText.Size = new System.Drawing.Size(1372, 313);
+            this.texto_richText.TabIndex = 3;
+            this.texto_richText.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1427, 788);
+            this.Controls.Add(this.texto_richText);
+            this.Controls.Add(this.listBox_variablesEncontradas);
+            this.Controls.Add(this.interpretar_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button interpretar_btn;
+        private System.Windows.Forms.ListBox listBox_variablesEncontradas;
+        private System.Windows.Forms.RichTextBox texto_richText;
+    }
+}
+
---
+++ AnalizadorLexico/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace AnalizadorLexico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MatchCollection matchsEncontraods;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox_variablesEncontradas.Items.Clear();
+            String texto = texto_richText.Text;
+            String expresionRegex = @"(const )([a-zA-z][a-zA-Z0-9]+)( *)(=)( *)((\+|\-|\*|\/)?( *)(\.[0-9]+|[0-9]+|[0-9]+\.[0-9]+)(( *)(\+|\-|\*|\/)( *)(\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+))+)+( *)(\;?)";
+
+            Console.WriteLine(texto);
+            matchsEncontraods = Regex.Matches(texto, expresionRegex);
+            foreach (Match match in matchsEncontraods)
+            {
+                Console.WriteLine(match.Value);
+                listBox_variablesEncontradas.Items.Add(match.Value);
+            }
+
+        }
+    }
+}
