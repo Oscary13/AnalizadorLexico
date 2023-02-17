3f817688816bf0b6afaa8e2e69bc4f8d0d6f9fc0
ERIKA TAMARA GARCIA <oscary_1320114020@uptecamac.edu.mx>
2023-02-16 18:48:56 -0600

    16-02-23

--- AnalizadorLexico/Form1.Designer.cs
+++ AnalizadorLexico/Form1.Designer.cs
@@ -29,30 +29,22 @@
         private void InitializeComponent()
         {
             this.interpretar_btn = new System.Windows.Forms.Button();
-            this.listBox_variablesEncontradas = new System.Windows.Forms.ListBox();
             this.texto_richText = new System.Windows.Forms.RichTextBox();
+            this.listBox_variablesEncontradas = new System.Windows.Forms.ListBox();
+            this.listBox_variablesOperaciones = new System.Windows.Forms.ListBox();
+            this.listBox_variablesDouble = new System.Windows.Forms.ListBox();
+            this.listBox_variablesEntero = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // interpretar_btn
             // 
-            this.interpretar_btn.Location = new System.Drawing.Point(91, 605);
+            this.interpretar_btn.Location = new System.Drawing.Point(28, 13);
             this.interpretar_btn.Name = "interpretar_btn";
             this.interpretar_btn.Size = new System.Drawing.Size(75, 23);
             this.interpretar_btn.TabIndex = 1;
             this.interpretar_btn.Text = "Interpretar";
             this.interpretar_btn.UseVisualStyleBackColor = true;
             this.interpretar_btn.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // listBox_variablesEncontradas
-            // 
-            this.listBox_variablesEncontradas.FormattingEnabled = true;
-            this.listBox_variablesEncontradas.HorizontalScrollbar = true;
-            this.listBox_variablesEncontradas.Location = new System.Drawing.Point(568, 404);
-            this.listBox_variablesEncontradas.MultiColumn = true;
-            this.listBox_variablesEncontradas.Name = "listBox_variablesEncontradas";
-            this.listBox_variablesEncontradas.ScrollAlwaysVisible = true;
-            this.listBox_variablesEncontradas.Size = new System.Drawing.Size(236, 277);
-            this.listBox_variablesEncontradas.TabIndex = 2;
             // 
             // texto_richText
             // 
@@ -62,13 +54,48 @@
             this.texto_richText.TabIndex = 3;
             this.texto_richText.Text = "";
             // 
+            // listBox_variablesEncontradas
+            // 
+            this.listBox_variablesEncontradas.FormattingEnabled = true;
+            this.listBox_variablesEncontradas.Location = new System.Drawing.Point(28, 381);
+            this.listBox_variablesEncontradas.Name = "listBox_variablesEncontradas";
+            this.listBox_variablesEncontradas.Size = new System.Drawing.Size(225, 290);
+            this.listBox_variablesEncontradas.TabIndex = 4;
+            // 
+            // listBox_variablesOperaciones
+            // 
+            this.listBox_variablesOperaciones.FormattingEnabled = true;
+            this.listBox_variablesOperaciones.Location = new System.Drawing.Point(269, 381);
+            this.listBox_variablesOperaciones.Name = "listBox_variablesOperaciones";
+            this.listBox_variablesOperaciones.Size = new System.Drawing.Size(231, 290);
+            this.listBox_variablesOperaciones.TabIndex = 5;
+            // 
+            // listBox_variablesDouble
+            // 
+            this.listBox_variablesDouble.FormattingEnabled = true;
+            this.listBox_variablesDouble.Location = new System.Drawing.Point(516, 381);
+            this.listBox_variablesDouble.Name = "listBox_variablesDouble";
+            this.listBox_variablesDouble.Size = new System.Drawing.Size(209, 290);
+            this.listBox_variablesDouble.TabIndex = 6;
+            // 
+            // listBox_variablesEntero
+            // 
+            this.listBox_variablesEntero.FormattingEnabled = true;
+            this.listBox_variablesEntero.Location = new System.Drawing.Point(743, 381);
+            this.listBox_variablesEntero.Name = "listBox_variablesEntero";
+            this.listBox_variablesEntero.Size = new System.Drawing.Size(200, 290);
+            this.listBox_variablesEntero.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1427, 788);
-            this.Controls.Add(this.texto_richText);
+            this.Controls.Add(this.listBox_variablesEntero);
+            this.Controls.Add(this.listBox_variablesDouble);
+            this.Controls.Add(this.listBox_variablesOperaciones);
             this.Controls.Add(this.listBox_variablesEncontradas);
+            this.Controls.Add(this.texto_richText);
             this.Controls.Add(this.interpretar_btn);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -78,8 +105,11 @@
 
         #endregion
         private System.Windows.Forms.Button interpretar_btn;
-        private System.Windows.Forms.ListBox listBox_variablesEncontradas;
         private System.Windows.Forms.RichTextBox texto_richText;
+        private System.Windows.Forms.ListBox listBox_variablesEncontradas;
+        private System.Windows.Forms.ListBox listBox_variablesOperaciones;
+        private System.Windows.Forms.ListBox listBox_variablesDouble;
+        private System.Windows.Forms.ListBox listBox_variablesEntero;
     }
 }
 
--- AnalizadorLexico/Form1.cs
+++ AnalizadorLexico/Form1.cs
@@ -24,24 +24,35 @@ namespace AnalizadorLexico
         private void button1_Click(object sender, EventArgs e)
         {
             listBox_variablesEncontradas.Items.Clear();
+            listBox_variablesDouble.Items.Clear();
+            listBox_variablesOperaciones.Items.Clear();
+            listBox_variablesEntero.Items.Clear();
             String texto = texto_richText.Text;
-            String expresionRegex_variableOperaciones = @"(const |let |var |int |double |float |decimal )([a-zA-z][a-zA-Z0-9]+)( *)(=)( *)((\+|\-|\*|\/)?( *)(\.[0-9]+|[0-9]+|[0-9]+\.[0-9]+)(( *)(\+|\-|\*|\/)( *)(\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+))+)+( *)(\;?)";
+            String expresionRegex_variableOperaciones = @"(const |let |var |int |double |float |decimal )([a-zA-z][a-zA-Z0-9]+)( *)(=)( *)((\+|\-|\*|\/)?( *)(\.[0-9]+|[0-9]+|[0-9]+\.[0-9]+)(( *)(\+|\-|\*|\/)( *)(\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+))+)+( *)(\;)";
             String expresionRegex_variablesDecimales = @"(const |let |var |double |float |decimal )([a-zA-z][a-zA-Z0-9]+)( *= *)( *)(\.[0-9]+|[0-9]+\.[0-9]+)(\;)";
-
+            String expresionRegex_variablesEnteros = @"(int |var |const )([a-zA-z][a-zA-Z0-9]+)( *= *)( *)([0-9]+)( *)(\;)";
+            
             matchsEncontraods = Regex.Matches(texto, expresionRegex_variableOperaciones);
             foreach (Match match in matchsEncontraods)
             {
-                Console.WriteLine(match.Value);
                 listBox_variablesEncontradas.Items.Add(match.Value);
+                listBox_variablesOperaciones.Items.Add(match.Value);
             }
 
             matchsEncontraods = Regex.Matches(texto, expresionRegex_variablesDecimales);
             foreach (Match match in matchsEncontraods)
             {
-                Console.WriteLine(match.Value);
                 listBox_variablesEncontradas.Items.Add(match.Value);
+                listBox_variablesDouble.Items.Add(match.Value);
             }
-            
+
+            matchsEncontraods = Regex.Matches(texto, expresionRegex_variablesEnteros);
+            foreach (Match match in matchsEncontraods)
+            {
+                listBox_variablesEncontradas.Items.Add(match.Value);
+                listBox_variablesEntero.Items.Add(match.Value);
+            }
+
 
         }
     }
