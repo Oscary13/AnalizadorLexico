fd00ad7a9f203bb5cdd3d8417a4d3f2897e9db2e
ERIKA TAMARA GARCIA <oscary_1320114020@uptecamac.edu.mx>
2023-02-16 18:23:40 -0600

    16-02-23

--- AnalizadorLexico/Form1.cs
+++ AnalizadorLexico/Form1.cs
@@ -25,15 +25,23 @@ namespace AnalizadorLexico
         {
             listBox_variablesEncontradas.Items.Clear();
             String texto = texto_richText.Text;
-            String expresionRegex = @"(const )([a-zA-z][a-zA-Z0-9]+)( *)(=)( *)((\+|\-|\*|\/)?( *)(\.[0-9]+|[0-9]+|[0-9]+\.[0-9]+)(( *)(\+|\-|\*|\/)( *)(\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+))+)+( *)(\;?)";
+            String expresionRegex_variableOperaciones = @"(const |let |var |int |double |float |decimal )([a-zA-z][a-zA-Z0-9]+)( *)(=)( *)((\+|\-|\*|\/)?( *)(\.[0-9]+|[0-9]+|[0-9]+\.[0-9]+)(( *)(\+|\-|\*|\/)( *)(\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+))+)+( *)(\;?)";
+            String expresionRegex_variablesDecimales = @"(const |let |var |double |float |decimal )([a-zA-z][a-zA-Z0-9]+)( *= *)( *)(\.[0-9]+|[0-9]+\.[0-9]+)(\;)";
 
-            Console.WriteLine(texto);
-            matchsEncontraods = Regex.Matches(texto, expresionRegex);
+            matchsEncontraods = Regex.Matches(texto, expresionRegex_variableOperaciones);
             foreach (Match match in matchsEncontraods)
             {
                 Console.WriteLine(match.Value);
                 listBox_variablesEncontradas.Items.Add(match.Value);
             }
+
+            matchsEncontraods = Regex.Matches(texto, expresionRegex_variablesDecimales);
+            foreach (Match match in matchsEncontraods)
+            {
+                Console.WriteLine(match.Value);
+                listBox_variablesEncontradas.Items.Add(match.Value);
+            }
+            
 
         }
     }
